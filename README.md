4e71ecdf95875e352df4895f4c3dd8bf4ada669f
Kyrylo Sovailo <k.sovailo@gmail.com>
2025-01-31 05:05:06 +0100

    Better visualizer

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-Proving that C++ is not slow, it's just a skill issue
+### Proving that C++ is not slow, it's just a skill issue
 
-![Bar chart with executions times](benchmark.png "Execution times")
+This repository contains implementations of Dijkstra algorithm in different programming languages. Quick peek at the results:
 
-### Overview
-This repository contains implementations of Dijkstra algorithm in different programming languages.
+![Bar chart with executions times, Matlab is the slowest, C with cheats is the fastest](benchmark.png "Execution times")
+All data is included, you are welcome to play with the configuration of the visualizer.
 
 ### Goal
 The benchmark is a combined benchmark that strives to capture a performance of a balanced program. The execution time is affected by:
@@ -15,7 +15,7 @@ The benchmark is a combined benchmark that strives to capture a performance of a
  - etc.
 This is also the reason why the programs don't reserve the memory in advance -- because frequent memory allocation is what applications do in real life. The programs (with one exception) are written with standard cross-platform I/O, no third-party libraries and no multithreading.
 
-### Bar chart
+### Notes
 The names of the bars on the chart are pretty self-explanatory. Except for:
  - `C, clang++, release` is a C program compiled with clang++ as valid C++
  - `C, clang, map+threads` is a C program written with multi-threading support and memory mapping enabled. It is written as a demonstration, comparing its performance to other programs is **not fair**.
--- create_graphics.py
+++ create_graphics.py
@@ -36,53 +36,118 @@ class Measurement:
         elif "Python" in labels: self.color = "cyan"
         elif "Matlab" in labels: self.color = "blue"
         else: self.color = "black"
-    
-def main(inverse):
-    label_to_filename = {
-        "C++, g++, debug" : "dijkstra_cpp_gcc_debug.txt",
-        "C++, g++, release" : "dijkstra_cpp_gcc_release.txt",
-        "C++, clang++, debug" : "dijkstra_cpp_clang_debug.txt",
-        "C++, clang++, release" : "dijkstra_cpp_clang_release.txt",
-        "C, gcc, debug" : "dijkstra_c_gcc_debug.txt",
-        "C, gcc, release" : "dijkstra_c_gcc_release.txt",
-        "C, clang, debug" : "dijkstra_c_clang_debug.txt",
-        "C, clang, release" : "dijkstra_c_clang_release.txt",
-        "C#, mcs, debug" : "dijkstra_csharp_mcs_debug.txt",
-        "C#, mcs, release" : "dijkstra_csharp_mcs_release.txt",
-        "Fortran, gfortran, debug" : "dijkstra_fortran_gfortran_debug.txt",
-        "Fortran, gfortran, release" : "dijkstra_fortran_gfortran_release.txt",
-        "Haskell, ghc, debug" : "dijkstra_haskell_ghc_debug.txt",
-        "Haskell, ghc, release" : "dijkstra_haskell_ghc_release.txt",
-        "Python" : "dijkstra_python.txt",
-        "Matlab" : "dijkstra_matlab_matlab.txt",
 
-        "C, clang, map+threads" : "dijkstra_c_clang_release_opt.txt",
-        "C, clang++, release" : "dijkstra_c_clang_release_cpp.txt",
-    }
-    measurements = []
-    for label, filename in label_to_filename.items(): measurements.append(Measurement(label, filename))
-    measurements.sort(key=lambda m: m.mean)
-    
-    labels = [ m.label for m in measurements ]
-    values = [ m.mean for m in measurements ] if not inverse else [ m.inv_mean for m in measurements ]
-    deviations = [ m.deviation for m in measurements ] if not inverse else [ m.inv_deviation for m in measurements ]
-    colors = [ m.color for m in measurements ]
+class Graphics:
+    def __init__(self):
+        self.inverse = False
+        self.reverse = False
+        self.log = False
+        self.no_extras = False
+        self.relative_fastest = False
+        self.relative_slowest = False
+        self.faster_threshold = math.inf
+        self.slower_threshold = -math.inf
 
-    title = "Execution time diagram" if not inverse else "Execution speed diagram"
-    figure, axes = plt.subplots(1, 1, num=title)
-    axes.bar(labels, values, yerr=deviations, color=colors)
-    for i, m in enumerate(measurements):
-        position = ((m.mean + m.deviation) if not inverse else (m.inv_mean + m.inv_deviation)) + max(values) / 100
-        text = f"{m.mean:0.2f}s" if not inverse else f"{m.inv_mean:0.3f}$s^{{-1}}$"
-        axes.text(i, position, text, ha="center")
-    axes.set_xticks(range(len(labels)))
-    axes.set_xticklabels(labels, rotation=15)
-    axes.set_title(title)
-    axes.set_xlabel("Language/compiler")
-    axes.set_ylabel("Time, s" if not inverse else "Speed, $s^{-1}$")
-    plt.show()
+        i = 1
+        while i < len(sys.argv):
+            arg = sys.argv[i]
+            if arg == "--help":
+                print("Usage: ./create_graphics.py [--help] [--inverse] [--reverse] [--log] [--no-extras] [--relative-fastest | --relative-slowest]  [--slower TIME] [--faster TIME]")
+                sys.exit(0)
+            elif arg == "--inverse": self.inverse = True
+            elif arg == "--reverse": self.reverse = True
+            elif arg == "--log": self.log = True
+            elif arg == "--no-extras": self.no_extras = True
+            elif arg == "--faster":
+                i += 1
+                if i >= len(sys.argv): raise Exception("Expected time after --faster")
+                self.faster_threshold = float(sys.argv[i])
+            elif arg == "--slower":
+                i += 1
+                if i >= len(sys.argv): raise Exception("Expected time after --slower")
+                self.slower_threshold = float(sys.argv[i])
+            elif arg == "--relative-fastest": self.relative_fastest = True
+            elif arg == "--relative-slowest": self.relative_slowest = True
+            else: raise Exception("Invalid argument")
+            i += 1
+                
+        if self.relative_slowest and self.relative_fastest: raise Exception("--relative-fastest incompatible with --relative-fastest")
+        if self.faster_threshold < self.slower_threshold: raise Exception("Maximum threshold is lower than minium threshold")
+
+    def run(self):
+        label_to_filename = {
+            "C++, g++, debug" : "dijkstra_cpp_gcc_debug.txt",
+            "C++, g++, release" : "dijkstra_cpp_gcc_release.txt",
+            "C++, clang++, debug" : "dijkstra_cpp_clang_debug.txt",
+            "C++, clang++, release" : "dijkstra_cpp_clang_release.txt",
+            "C, gcc, debug" : "dijkstra_c_gcc_debug.txt",
+            "C, gcc, release" : "dijkstra_c_gcc_release.txt",
+            "C, clang, debug" : "dijkstra_c_clang_debug.txt",
+            "C, clang, release" : "dijkstra_c_clang_release.txt",
+            "C#, mcs, debug" : "dijkstra_csharp_mcs_debug.txt",
+            "C#, mcs, release" : "dijkstra_csharp_mcs_release.txt",
+            "Fortran, gfortran, debug" : "dijkstra_fortran_gfortran_debug.txt",
+            "Fortran, gfortran, release" : "dijkstra_fortran_gfortran_release.txt",
+            "Haskell, ghc, debug" : "dijkstra_haskell_ghc_debug.txt",
+            "Haskell, ghc, release" : "dijkstra_haskell_ghc_release.txt",
+            "Python" : "dijkstra_python.txt",
+            "Matlab" : "dijkstra_matlab_matlab.txt"
+        }
+        if not self.no_extras:
+            extras = {
+                "C, clang, map+threads" : "dijkstra_c_clang_release_opt.txt",
+                "C, clang++, release" : "dijkstra_c_clang_release_cpp.txt"
+            }
+            label_to_filename.update(extras)
+
+        measurements = []
+        for label, filename in label_to_filename.items():
+            m = Measurement(label, filename)
+            if m.mean <= self.faster_threshold and m.mean >= self.slower_threshold: measurements.append(m)
+        if not self.reverse: measurements.sort(key=lambda m: m.mean)
+        else: measurements.sort(key=lambda m: -m.mean)
+        
+        labels = [ m.label for m in measurements ]
+        values = [ m.mean for m in measurements ] if not self.inverse else [ m.inv_mean for m in measurements ]
+        deviations = [ m.deviation for m in measurements ] if not self.inverse else [ m.inv_deviation for m in measurements ]
+        colors = [ m.color for m in measurements ]
+
+        if self.relative_fastest:
+            fastest_i = -1 if self.reverse else 0
+            fastest = values[fastest_i]
+            for i in range(len(values)):
+                values[i] /= fastest
+                deviations[i] /= fastest
+            values[fastest_i] = 1
+        elif self.relative_slowest:
+            slowest_i = 0 if self.reverse else -1
+            slowest = values[slowest_i]
+            for i in range(len(values)):
+                values[i] /= slowest
+                deviations[i] /= slowest
+            values[slowest_i] = 1
+
+        title = "Execution time diagram" if not self.inverse else "Execution speed diagram"
+        figure, axes = plt.subplots(1, 1, num=title)
+        axes.bar(labels, values, yerr=deviations, color=colors)
+        axes.set_title(title)
+        for i, (value, deviation) in enumerate(zip(values, deviations)):
+            position = value + deviation
+            if self.log: position *= math.exp(math.log(max(values)) / 100)
+            else: position += max(values) / 100
+            if self.relative_fastest or self.relative_slowest: text = f"{value:0.2f}x" if self.relative_slowest == self.inverse else f"{1/value:0.2f}x"
+            else: text = f"{value:0.2f}s" if not self.inverse else f"{value:0.3f}$s^{{-1}}$"
+            axes.text(i, position, text, ha="center")
+        axes.set_xlabel("Language/compiler")
+        axes.set_xticks(range(len(labels)))
+        axes.set_xticklabels(labels, rotation=15)
+        axes.set_ylabel("Time, s" if not self.inverse else "Speed, $s^{-1}$")
+        if self.log: axes.set_yscale('log')
+        plt.show()
+
+def main():
+    graphics = Graphics()
+    graphics.run()
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 2 and sys.argv[1] == "--inverse": main(True)
-    elif len(sys.argv) == 1: main(False)
-    else: raise Exception("Invalid usage")
+    main()
