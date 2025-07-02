0c4fe2b8abdcdfdbc7824a9b44c9c39e176c3273
Kyrylo Sovailo <k.sovailo@gmail.com>
2025-07-02 17:49:35 +0200

    Merged old and vew version

--- source/dijkstra_c_freestanding_asm.c
+++ source/dijkstra_c_freestanding_asm.c
@@ -10,6 +10,8 @@
     #define pointer_syscall ((pointer_syscall_t*)(&syscall))
 #endif
 
+#undef USE_UINT_MULTIPLICATION
+
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef signed short int16_t;
@@ -469,6 +471,7 @@ static float push_connection(struct ConnectionVector *connections, uint64_t dest
     return distance;
 }
 
+#ifdef USE_UINT_MULTIPLICATION
 //uint64 can represent integers up to 19'th order (inclusive) (10^19)
 //float32 can represent floats down to -45'th order (inclusive) (10^-45)
 static float order_table[20] = {
@@ -493,16 +496,21 @@ static float order_table[20] = {
     1e-18f,
     1e-19f //19th
 };
+#endif
 
+#ifdef USE_UINT_MULTIPLICATION
 static float push_bi_connection(struct ConnectionVectorVector *connections, uint64_t source, uint64_t destination,
     uint64_t distance_integral, uint64_t distance_fraction, uint64_t distance_order)
 {
+#else
+static float push_bi_connection(struct ConnectionVectorVector *connections, uint64_t source, uint64_t destination, float distance)
+{
+#endif
     //subfunctions are relatively rarely called, prioritize scratch registers
 
     //connections - RAX
     //source - ESI, stack
     //destination - EDX, stack
-    float distance; //XMM0
     uint32_t source_destination_max; //EDX
     uint32_t new_connections_length; //EDX
     struct ConnectionVector *connections_begin; //R8
@@ -513,7 +521,12 @@ static float push_bi_connection(struct ConnectionVectorVector *connections, uint
     struct ConnectionVector *dst; //RDI
     //i - RCX
 
+    #ifdef USE_UINT_MULTIPLICATION
+    float distance; //XMM0
     distance = (float)distance_integral + (float)distance_fraction * order_table[distance_order];
+    #else
+    //distance - XMM0
+    #endif
 
     connections_begin = connections->begin; //borrow connections->begin
     source_destination_max = (uint32_t)((source > destination) ? source : destination);
@@ -680,19 +693,19 @@ enum state
 
 static void parse_ver5(struct ConnectionVectorVector *graph, struct BenchmarkVector *benchmarks)
 {
-    //graph - R14
-    //benchmarks - stack [rsp]
+    //graph - stack (R14?)
+    //benchmarks - stack (R15?)
 
     //I/O
     int64_t code; //RAX
-    int64_t file; //RAX, stack [rsp + 8]
+    int64_t file; //RAX, stack [rsp]
     char c; //AL
     char *p; //R12
     bool eof; //R13b, 0x80 mask
     #ifdef USE_MAPPING
     char *buffer;
     #else
-    char buffer[IO_BUFFER_SIZE]; //stack [rbp - 0x10000]==[rsp + 0x38] to [rbp]==[rsp + 0x10038]
+    char buffer[IO_BUFFER_SIZE]; //stack [rbp - 0x10000]==[rsp + 0x30] to [rbp]==[rsp + 0x10030]
     #endif
     char *buffer_end; //RBP
 
@@ -700,11 +713,17 @@ static void parse_ver5(struct ConnectionVectorVector *graph, struct BenchmarkVec
     enum state state; //RBX
     bool read_benchmarks; //R13b, 0x40 mask
     bool delayed_push; //R13b, 0x20 mask
-    uint64_t source; //RSI, stack [rsp + 16]
-    uint64_t destination; //RDX, stack [rsp + 24]
+    uint64_t source; //RSI, stack [rsp + 8]
+    uint64_t destination; //RDX, stack [rsp + 16]
+    #ifdef USE_UINT_MULTIPLICATION
+    //Stack positions were lost in merged version, see commit bbfee9c26f757eb9afbf537556dfc9359e9bfbfe for original values
     uint64_t distance_integral; //RCX, stack [rsp + 32]
     uint64_t distance_fraction; //R8, stack [rsp + 40]
     unsigned char distance_order; //R9, stack [rsp + 48]
+    #else
+    float distance; //XMM0, [rsp + 24], integer part in RCX, [rsp+32]
+    float order;    //XMM1, [rsp + 28]
+    #endif
     uint8_t keyword_check; //RSI
     
     //I/O
@@ -800,11 +819,16 @@ static void parse_ver5(struct ConnectionVectorVector *graph, struct BenchmarkVec
             if (eof) state = st_endfile; //EOF -> syntax error, end
             else if (c == ' ' || c == '\t') {} //space -> repeat
             else if (c == '\n' || c == '\r') state = st_endfile; //endline -> format error, end
+            #ifdef USE_UINT_MULTIPLICATION
             else if (c >= '0' && c <= '9') { distance_integral = (unsigned char)(c - '0'); state = st_search_distance_dot; } //number -> set distance, search distance dot
+            #else
+            else if (c >= '0' && c <= '9') { distance = (unsigned char)(c - '0'); state = st_search_distance_dot; } //number -> set distance, search distance dot
+            #endif
             else state = st_endfile; //else -> format error, end
             break;
 
         case st_search_distance_dot:
+            #ifdef USE_UINT_MULTIPLICATION
             if (eof) { push_bi_connection(graph, source, destination, distance_integral, 0, 0); state = st_endfile; } //EOF -> push connection, end
             else if (c == ' ' || c == '\t') { distance_fraction = 0; distance_order = 0; delayed_push = 1; state = st_search_endline; } //space -> delayed push connection, search endline
             else if (c == '\n' || c == '\r') { push_bi_connection(graph, source, destination, distance_integral, 0, 0); state = st_search_source_begin; } //endline -> push connection, start over
@@ -812,14 +836,32 @@ static void parse_ver5(struct ConnectionVectorVector *graph, struct BenchmarkVec
             else if (c == '.') { distance_fraction = 0; distance_order = 0; state = st_search_distance_end; } //dot -> set order, search distance end
             else state = st_endfile; //else -> format error, end
             break;
+            #else
+            if (eof) { push_bi_connection(graph, source, destination, distance); state = st_endfile; } //EOF -> push connection, end
+            else if (c == ' ' || c == '\t') { delayed_push = 1; state = st_search_endline; } //space -> delayed push connection, search endline
+            else if (c == '\n' || c == '\r') { push_bi_connection(graph, source, destination, distance); state = st_search_source_begin; } //endline -> push connection, start over
+            else if (c >= '0' && c <= '9') distance = 10 * distance + (unsigned char)(c - '0'); //number -> modify distance, repeat
+            else if (c == '.') { order = 1; state = st_search_distance_end; } //dot -> set order, search distance end
+            else state = st_endfile; //else -> format error, end
+            break;
+            #endif
 
         case st_search_distance_end:
+            #ifdef USE_UINT_MULTIPLICATION
             if (eof) { push_bi_connection(graph, source, destination, distance_integral, distance_fraction, distance_order); state = st_endfile; } //EOF -> push connection, end
             else if (c == ' ' || c == '\t') { delayed_push = 1; state = st_search_endline; } //space -> delayed push connection, search endline
             else if (c == '\n' || c == '\r') { push_bi_connection(graph, source, destination, distance_integral, distance_fraction, distance_order); state = st_search_source_begin; } //endline -> push connection, start over
             else if (c >= '0' && c <= '9') { distance_fraction += 10 * distance_fraction + (unsigned char)(c - '0'); distance_order++; } //number -> modify distance, repeat
             else state = st_endfile; //else -> format error, end
             break;
+            #else
+            if (eof) { push_bi_connection(graph, source, destination, distance); state = st_endfile; } //EOF -> push connection, end
+            else if (c == ' ' || c == '\t') { delayed_push = 1; state = st_search_endline; } //space -> delayed push connection, search endline
+            else if (c == '\n' || c == '\r') { push_bi_connection(graph, source, destination, distance); state = st_search_source_begin; } //endline -> push connection, start over
+            else if (c >= '0' && c <= '9') { order /= 10; distance += order * (unsigned char)(c - '0'); } //number -> modify distance, repeat
+            else state = st_endfile; //else -> format error, end
+            break;
+            #endif
         
         case st_search_graph_end:
             if (eof) state = st_endfile; //EOF -> format error, end
@@ -845,14 +887,22 @@ static void parse_ver5(struct ConnectionVectorVector *graph, struct BenchmarkVec
             if (eof) //EOF -> end
             {
                 if (delayed_push && read_benchmarks) push_benchmark(benchmarks, source, destination);
+                #ifdef USE_UINT_MULTIPLICATION
                 else if (delayed_push && !read_benchmarks) push_bi_connection(graph, source, destination, distance_integral, distance_fraction, distance_order);
+                #else
+                else if (delayed_push && !read_benchmarks) push_bi_connection(graph, source, destination, distance);
+                #endif
                 state = st_endfile;
             }
             else if (c == ' ' || c == '\t') {} //space -> repeat
             else if (c == '\n' || c == '\r') //endline -> start over
             {
                 if (delayed_push && read_benchmarks) { push_benchmark(benchmarks, source, destination); delayed_push = 0; }
+                #ifdef USE_UINT_MULTIPLICATION
                 else if (delayed_push && !read_benchmarks) { push_bi_connection(graph, source, destination, distance_integral, distance_fraction, distance_order); delayed_push = 0; }
+                #else
+                else if (delayed_push && !read_benchmarks) { push_bi_connection(graph, source, destination, distance); delayed_push = 0; }
+                #endif
                 state = st_search_source_begin;
             }
             else state = st_endfile; //else -> format error, end
